85b01df6860238ada810086af592d91f0f77a563
1250687304 <1250687304@qq.com>
2019-11-25 21:56:21 +0800

    删除多余框架

--- iThinkapp/app/src/main/java/com/dream/mvpdemo/model/http/AppApiHelper.java
+++
@@ -1,9 +0,0 @@
-package com.dream.mvpdemo.model.http;
-
-/**
- * Created by Administrator on 2018/5/8.
- */
-
-public interface AppApiHelper {
-    void testRequestNetwork();
-}
--- iThinkapp/app/src/main/java/com/dream/mvpdemo/model/preference/AppPreferenceHelper.java
+++
@@ -1,9 +0,0 @@
-package com.dream.mvpdemo.model.preference;
-
-/**
- * Created by Administrator on 2018/5/8.
- */
-
-public interface AppPreferenceHelper {
-    void testPreference();
-}
