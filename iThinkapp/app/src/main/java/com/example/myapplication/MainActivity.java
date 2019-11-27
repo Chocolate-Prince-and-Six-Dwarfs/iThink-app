a325eede46c7c72a2c42ead0796d44b5f49f7014
1250687304 <1250687304@qq.com>
2019-11-27 19:22:42 +0800

    登录验证

--- iThinkapp/app/src/main/java/com/example/myapplication/MainActivity.java
+++ iThinkapp/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,26 +2,17 @@ package com.example.myapplication;
 
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import android.widget.Toast;
-
 import com.example.myapplication.model.http.ApiHelper;
-
 import okhttp3.FormBody;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import okhttp3.RequestBody;
-import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -40,21 +31,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         editText1 = (EditText)findViewById(R.id.et_user_name);
         editText2 = (EditText)findViewById(R.id.et_psw);
-        Log.d("MainActivity","1");
+
     }
 
     @Override
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.btn_login:
-                Intent intent = new Intent();
-                intent.setData(Uri.parse("http://47.97.187.33:8080"));
-                intent.setAction(Intent.ACTION_VIEW);
-                this.startActivity(intent);
+                RequestBody requestBody = new FormBody.Builder()
+                    .add("email", editText1.getText().toString())
+                    .add("pwd",editText2.getText().toString())
+                    .build();
+                ApiHelper.sendHttpRequest("http://47.97.187.33:8080/user/login",requestBody);
                 break;
+            case R.id.btn_enter:
+                Intent intent = new Intent("android.intent.action.SECOND");
+                startActivity(intent);
             default:
                 break;
 
         }
     }
+
+
+
 }
