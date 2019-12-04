4cab8136e96b4813b2a596aecdac18f3241a4184
Shen-MoMo <644245985@qq.com>
2019-12-04 14:25:16 +0800

    删除代码

--- iThinkapp/app/src/main/java/com/example/myapplication/CreateIdeaActivity.java
+++ iThinkapp/app/src/main/java/com/example/myapplication/CreateIdeaActivity.java
@@ -115,10 +115,4 @@ public class CreateIdeaActivity extends AppCompatActivity implements View.OnClic
             }
         });
     }
-
-    public void initUser()
-    {
-        user.setId(0);
-        user.setName("游客");
-    }
 }
